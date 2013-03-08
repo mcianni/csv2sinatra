49abc8547dca4e9ecd92c8e1556ab203180bfc11
mcianni <mcianni@gmail.com>
2013-03-07 19:00:09 -0500

    refactor

--- models/base.rb
+++ models/base.rb
@@ -9,9 +9,7 @@ module DynamicClasses
   # If yes, create and return the class
   # If no, pass request along
   def self.const_missing(c)
-    tables = repository(:default).adapter.select("SELECT name FROM sqlite_master\
-                                         WHERE type in ('table', 'view')\
-                                         AND name not like '%sqlite_%'")
+    tables = db_tables
     
     if tables.include?(DataMapper::Inflector.tableize(c.to_s))
       # TODO : refactor this
@@ -31,18 +29,13 @@ module DynamicClasses
   end
 
   def self.list
-    tables = repository(:default).adapter.select("SELECT name FROM sqlite_master\
-                                                  WHERE type in ('table', 'view')\
-                                                  AND name not like '%sqlite_%'")
+    tables = db_tables
     tables.map{ |t| DataMapper::Inflector.classify(t) }
   end
 
   def self.load_classes(table_name=nil)
-    tables = table_name ? 
-                [table_name] : 
-                repository(:default).adapter.select("SELECT name FROM sqlite_master\
-                                                     WHERE type in ('table', 'view')\
-                                                     AND name not like '%sqlite_%'")
+    tables = table_name ? [table_name] : db_tables
+
     tables.each do |table|
       klass = Class.new(DynamicClasses::Base)
       Object.const_set(DataMapper::Inflector.classify(table), klass)
@@ -53,6 +46,12 @@ module DynamicClasses
         klass.property(col, col == "id" ? klass::Serial : klass::Text)
       end
     end
+  end
+
+  def self.db_tables
+    repository(:default).adapter.select("SELECT name FROM sqlite_master\
+                                         WHERE type in ('table', 'view')\
+                                         AND name not like '%sqlite_%'")
   end
 
   self.load_classes
